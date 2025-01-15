0c3aabf1faa288dc154483180dc54dddda278327
Ziggismalls <frantz.o@icloud.com>
2025-01-15 11:58:58 +0100

    Last changes 15/1

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-// const divListVols = document.createElement("select");
-const divListeVols = document.createElement("div");
-const container = document.createElement("div");
+// modif
 const body = document.querySelector("body");
+const divSearch = document.createElement("div");
+const search = document.createElement("input");
+const buttonSeach = document.createElement("button");
+
+search.type = "search";
+divSearch.className = "searchListeVols";
+
+buttonSeach.addEventListener("click", function () {
+  const selectedCity = search.value;
+  console.log(selectedCity);
+
+  // on cherche le code ICAO  des aeroports de la ville selectionnée
+});
+
+divSearch.append(search);
+divSearch.append(buttonSeach);
+
+body.append(divSearch);
+// modif
+
+const divListeVols = document.createElement("select");
+const firstOption = document.createElement("option");
+divListeVols.appendChild(firstOption);
+firstOption.value = "-";
+// const divListeVols = document.createElement("div");
+const container = document.createElement("div");
 const countryVol = document.createElement("div");
 const airportInfo = document.createElement("div");
 
-divListeVols.className = "listeVols";
+divListeVols.className = "styleListeVols";
 countryVol.className = "country";
 container.className = "container";
 
@@ -24,19 +48,13 @@ fetch(url)
     return response.json();
   })
   .then((data) => {
-    // console.log(data);
-
-    // pour chaque vol on rfécupere les ICAO  des aéroports d'arrivée
     data.forEach((vol, index) => {
       let arrival = vol.estArrivalAirport;
-      // console.log(index);
-      // console.log(data);
-
       let url2 = "https://airportdb.io/api/v1/airport/";
       url2 +=
         arrival.toString() +
         "?apiToken=65eacd634d3a962e1f14193e000540024c2fd4818d02b281a09cd5000d7c02f1a857bfabf62e595f1ba161b99a7f4059";
-      // console.log(url2);
+
       fetch(url2)
         .then((response) => {
           return response.json();
@@ -45,33 +63,125 @@ fetch(url)
           let name = data2.name;
           let country = data2.country.name;
 
-          console.log(name);
-          // const volElement = document.createElement("option");
-          const volElement = document.createElement("div");
-          volElement.className = "volCarte";
+          const volElement = document.createElement("option");
 
-          // volElement.innerText = name;
-          volElement.innerHTML = name;
-          volElement.addEventListener("click", function () {
-            countryVol.innerHTML = "";
-            const para1 = document.createElement("p");
-            const para2 = document.createElement("p");
-            para1.className = "elemCountry";
-            para1.innerText = country;
-            countryVol.append(para1);
+          volElement.innerText = name;
 
-            para2.className = "elemMunicipality";
-            para2.innerText = data2.municipality;
-            countryVol.append(para2);
+          const infoDivListeVols = [];
 
-            divListeVols.append(countryVol);
-            container.append(countryVol);
-          });
+          // enregistrer la data requis pour le traitement des élements de la liste déroulante
+          infoDivListeVols.push(country);
+          infoDivListeVols.push(data2.municipality);
+          infoDivListeVols.push(data2.latitude_deg);
+          infoDivListeVols.push(data2.longitude_deg);
+          infoDivListeVols.push(data2.region.name);
+          infoDivListeVols.push(data2.home_link);
+          infoDivListeVols.push(data2.iso_country);
+
+          volElement.value = infoDivListeVols;
+
           divListeVols.append(volElement);
+          container.append(divListeVols);
+          body.append(container);
         });
     });
   })
   .catch(function () {});
 
-container.append(divListeVols);
-body.append(container);
+const divInfo = document.createElement("div");
+divInfo.className = "styleInfoContainer";
+
+divListeVols.addEventListener("change", function () {
+  const para1 = document.createElement("p");
+  const para2 = document.createElement("p");
+
+  const para3 = document.createElement("p");
+  const para4 = document.createElement("a");
+
+  // j'ai ajouté la const en dessous des autre parag
+  const imgFlag = document.createElement("img");
+
+  countryVol.innerHTML = "";
+  para1.className = "elemCountry";
+
+  // On reécupere la data de l'élement!
+
+  let params = event.target.value.split(",");
+
+  para1.append(document.createTextNode(params[0]));
+  countryVol.append(para1);
+
+  /* jai ajouté la suite en dessous du para1 pour afficher le drapeau juste en dessous du pays */
+  let flag = params[6];
+  imgFlag.src = "https://flagcdn.com/80x60/" + flag.toLowerCase() + ".png";
+  countryVol.append(imgFlag);
+
+  para2.className = "elemMunicipality";
+  para2.innerText = params[1];
+  countryVol.append(para2);
+
+  // et je les ai renseignée
+  para3.className = "elemMunicipality";
+  para3.innerText = "Region : " + params[4];
+  countryVol.append(para3);
+
+  para4.className = "elemMunicipality";
+  para4.innerText = "lien : " + params[5];
+  para4.href = params[5];
+  countryVol.append(para4);
+
+  //ffichage info mètèo
+
+  const divCity = document.createElement("div");
+  divCity.className = "styleCity";
+  divCity.append(document.createTextNode(params[1]));
+  divInfo.innerHTML = "";
+  divInfo.append(divCity);
+
+  const APIKey = "599f6dc440cb41b80f9690764858cb10";
+  const city = params[1];
+  const lat = params[2];
+  const lon = params[3];
+  // const APICall1 = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${APIKey}`;
+
+  // fetch(APICall1)
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  // const lat = data[0].lat;
+  // const lon = data[0].lon;
+  const APICall2 = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIKey}&units=metric`;
+
+  fetch(APICall2)
+    .then((response) => response.json())
+    .then((data) => {
+      // console.log(data);
+
+      const divTemperature = document.createElement("div");
+      divTemperature.className = "styleTemperature";
+      divTemperature.append(document.createTextNode(data.main.temp + " °C"));
+
+      divInfo.append(divTemperature);
+
+      const divHumidité = document.createElement("div");
+      divHumidité.className = "styleHumidité";
+      divHumidité.append(document.createTextNode(data.main.humidity + " %"));
+
+      divInfo.append(divHumidité);
+
+      const divVent = document.createElement("div");
+      divVent.className = "styleVent";
+      divVent.append(document.createTextNode(data.wind.speed + " km/h"));
+      const wind = document.createElement("img");
+      wind.src = "assets/wind-solid.svg";
+      divVent.append(wind);
+
+      divInfo.append(divVent);
+    });
+  // });
+
+  body.append(divInfo);
+  //affichage info mètèo
+
+  divListeVols.append(countryVol);
+  container.append(countryVol);
+});
